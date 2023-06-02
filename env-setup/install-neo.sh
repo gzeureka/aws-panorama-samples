79c92d9ef4ddfd1132d8afa658b248d1dba5c38c
gzeureka <gzeureka@gmail.com>
2023-06-02 10:39:21 +0800

    Change setup template and script

--- env-setup/install-neo.sh
+++ env-setup/install-neo.sh
@@ -2,7 +2,8 @@
   
                 sudo apt-get update
                 sudo apt-get install -y python3 python3-distutils build-essential cmake curl ca-certificates
-                curl https://bootstrap.pypa.io/get-pip.py -o /tmp/get-pip.py
+                # curl https://bootstrap.pypa.io/get-pip.py -o /tmp/get-pip.py
+                curl https://bootstrap.pypa.io/pip/3.6/get-pip.py -o /tmp/get-pip.py
                 sudo python3 /tmp/get-pip.py
                 rm /tmp/get-pip.py
                 sudo pip3 install -U pip setuptools wheel
